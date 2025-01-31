5a4c3ee898f14348b7eb26c9d31481aea2467751
Muhamad Arsaludin <muhamadarsaludin71@gmail.com>
2025-01-31 20:29:48 +0700

    adjust routes

--- src/api/collaborations/routes.js
+++ src/api/collaborations/routes.js
@@ -2,7 +2,7 @@ const routes = (handler) => [
   {
     method: 'POST',
     path: '/collaborations',
-    handler: (request, h) => handler.postCollaborationHandler(request, h),
+    handler: handler.postCollaborationHandler,
     options: {
       auth: 'openmusic_jwt',
     },
@@ -10,7 +10,7 @@ const routes = (handler) => [
   {
     method: 'DELETE',
     path: '/collaborations',
-    handler: (request) => handler.deleteCollaborationHandler(request),
+    handler: handler.deleteCollaborationHandler,
     options: {
       auth: 'openmusic_jwt',
     },
