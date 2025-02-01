b4dd6e01cc3ff156c9dc95e9b041b192b8c76eaa
Muhamad Arsaludin <muhamadarsaludin71@gmail.com>
2025-02-01 13:17:02 +0700

    adjust using config

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 const ClientError = require('./exceptions/ClientError');
+const config = require('./utils/config');
 // Albums
 const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
@@ -46,8 +47,8 @@ const init = async () => {
   const playlistSongActivitiesService = new PlaylistSongActivitiesService();
 
   const server = Hapi.server({
-    port: process.env.PORT,
-    host: process.env.HOST,
+    port: config.app.port,
+    host: config.app.host,
     routes: {
       cors: {
         origin: ['*'],
